1af1150d4cfc87bdae64792444f3bf2a76fce120
rajeev-rai-oc <rajeev.rai@oneconvergence.com>
2024-07-29 15:43:14 +0530

    Go routine demo

---
+++ goroutine.go
@@ -0,0 +1,36 @@
+// A _goroutine_ is a lightweight thread of execution.
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func f(from string) {
+	for i := 0; i < 3; i++ {
+		fmt.Println(from, ":", i)
+	}
+}
+
+func main() {
+
+	f("direct")
+
+	// To invoke this function in a goroutine, use
+	// `go f(s)`. This new goroutine will execute
+	// concurrently with the calling one.
+	go f("goroutine")
+
+	// You can also start a goroutine for an anonymous
+	// function call.
+	go func(msg string) {
+		fmt.Println(msg)
+	}("going :")
+
+	func(){
+		fmt.Println("Running Concurrently:")
+	}()
+
+	time.Sleep(time.Second)
+	fmt.Println("done")
+}
